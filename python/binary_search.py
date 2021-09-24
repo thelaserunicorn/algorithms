157a15347d1e07c531f036cb270d4ad3fb813b33
thelaserunicorn <rashideram2@gmail.com>
2021-09-24 18:29:59 +0600

    binary_search

--- python/binary_search.py
+++ python/binary_search.py
@@ -8,7 +8,6 @@ def binary_search(arr,x):
     while l<=r:
         
         mid = l + (r-l)//2
-
         if arr[mid] == x:
             return mid
         
@@ -16,6 +15,7 @@ def binary_search(arr,x):
             l = mid + 1;
         else:
             r = mid - 1
+            
     return - 1
 
 arr = [3,4,6,8,10]
